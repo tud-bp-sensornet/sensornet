315a820b54e030245d1adcca935f31456c8bc6e2
Teriame <richard_peter.mitev@stud.tu-darmstadt.de>
2015-06-29 18:12:38 +0200

    changed tabs to spaces and spaces to tabs

--- src/broadcast.c
+++ src/broadcast.c
@@ -1,30 +1,28 @@
 /**
- * \file
- * 			Identified broadcast Rime layer ontop of anonymous broadcast (abc)
- * \author
- * 			tud-bp-sensornet
+ * \file           Identified broadcast Rime layer ontop of anonymous broadcast (abc)
+ * \author         tud-bp-sensornet
  */
 
 #include "contiki-net.h"
 #include <string.h>
 
 static const struct packetbuf_attrlist attributes[] =
-  {
-    BROADCAST_ATTRIBUTES PACKETBUF_ATTR_LAST
-  };
+{
+	BROADCAST_ATTRIBUTES PACKETBUF_ATTR_LAST
+};
 
 /*---------------------------------------------------------------------------*/
 static void
 recv_from_abc(struct abc_conn *bc)
 {
-  rimeaddr_t sender;
-  struct p_broadcast_conn *c = (struct p_broadcast_conn *)bc;
+	rimeaddr_t sender;
+	struct p_broadcast_conn *c = (struct p_broadcast_conn *)bc;
 
-  rimeaddr_copy(&sender, packetbuf_addr(PACKETBUF_ADDR_SENDER));
-  
-  if(c->received) {
-    c->received(c, &sender, packetbuf_dataptr(), (size_t)packetbuf_datalen());
-  }
+	rimeaddr_copy(&sender, packetbuf_addr(PACKETBUF_ADDR_SENDER));
+
+	if (c->received) {
+		c->received(c, &sender, packetbuf_dataptr(), (size_t)packetbuf_datalen());
+	}
 }
 /*---------------------------------------------------------------------------*/
 static const struct abc_callbacks broadcast = {recv_from_abc};
@@ -32,24 +30,24 @@ static const struct abc_callbacks broadcast = {recv_from_abc};
 void
 p_broadcast_open(struct p_broadcast_conn *c, uint16_t channel)
 {
-  abc_open(&c->abc, channel, &broadcast);
-  channel_set_attributes(channel, attributes);
+	abc_open(&c->abc, channel, &broadcast);
+	channel_set_attributes(channel, attributes);
 }
 /*---------------------------------------------------------------------------*/
 void
 p_broadcast_close(struct p_broadcast_conn *c)
 {
-  abc_close(&c->abc);
+	abc_close(&c->abc);
 }
 /*---------------------------------------------------------------------------*/
 int
 p_broadcast_send(struct p_broadcast_conn *c, void *data, size_t length)
 {
-  packetbuf_clear ();
-  packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &rimeaddr_node_addr);
-  if(packetbuf_copyfrom(data, (uint16_t)length) != (int)length){
-    //TODO:Errorhandling
-  }
-  return abc_send(&c->abc);
+	packetbuf_clear ();
+	packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &rimeaddr_node_addr);
+	if (packetbuf_copyfrom(data, (uint16_t)length) != (int)length) {
+		//TODO:Errorhandling
+	}
+	return abc_send(&c->abc);
 }
 /*---------------------------------------------------------------------------*/
--- src/broadcast.h
+++ src/broadcast.h
@@ -1,8 +1,6 @@
 /**
- * \file
- * 			Header file for a identified broadcast Rime layer ontop of Anonymous BroadCast (abc)
- * \author
- * 			tud-bp-sensornet
+ * \file           Header file for a identified broadcast Rime layer ontop of Anonymous BroadCast (abc)
+ * \author         tud-bp-sensornet
  */
 
 #ifndef __BROADCAST_H__
@@ -13,59 +11,58 @@ struct p_broadcast_conn;
 #define BROADCAST_ATTRIBUTES  { PACKETBUF_ADDR_SENDER, PACKETBUF_ADDRSIZE }, ABC_ATTRIBUTES
 
 /**
- * \brief			Connection for broadcast including callback function
- * \param bc		A pointer to a struct p_broadcast_conn
- * \param sender	The rimeaddr_t of the sender of this data
- * \param data		A pointer to the data part of the packetbuf
- * \param length	The size of the data part of the packetbuf
+ * \brief           Connection for broadcast including callback function
+ * \param bc        A pointer to a struct p_broadcast_conn
+ * \param sender    The rimeaddr_t of the sender of this data
+ * \param data      A pointer to the data part of the packetbuf
+ * \param length    The size of the data part of the packetbuf
  */
 struct p_broadcast_conn {
-    struct abc_conn abc;
+	struct abc_conn abc;
 
-    void (*received)(const struct p_broadcast_conn *bc, const rimeaddr_t *sender, void *data, size_t length);
+	void (*received)(const struct p_broadcast_conn *bc, const rimeaddr_t *sender, void *data, size_t length);
 };
 
 /**
- * \brief			Set up an identified best-effort broadcast connection
- * \param c			A pointer to a struct p_broadcast_conn including a function pointer to a function that will be called when a packet has been received
- * \param channel	The channel on which the connection will operate
+ * \brief           Set up an identified best-effort broadcast connection
+ * \param c         A pointer to a struct p_broadcast_conn including a function
+ *                  pointer to a function that will be called when a packet has been received
+ * \param channel   The channel on which the connection will operate
  *
- * 					This function sets up a broadcast connection on the
- * 					specified channel. The caller must have allocated the
- * 					memory for the struct broadcast_conn, usually by declaring it
- * 					as a static variable.
+ *                  This function sets up a broadcast connection on the
+ *                  specified channel. The caller must have allocated the
+ *                  memory for the struct broadcast_conn, usually by declaring it
+ *                  as a static variable.
  *
- * 					The struct p_broadcast_conn pointer must point to a structure
- * 					containing a pointer to a function that will be called
- * 					when a packet arrives on the channel.
+ *                  The struct p_broadcast_conn pointer must point to a structure
+ *                  containing a pointer to a function that will be called
+ * 	                when a packet arrives on the channel.
  */
 void p_broadcast_open(struct p_broadcast_conn *c, uint16_t channel);
 
 /**
- * \brief			Close a broadcast connection
- * \param c			A pointer to a struct p_broadcast_conn
+ * \brief           Close a broadcast connection
+ * \param c         A pointer to a struct p_broadcast_conn
  *
- * 					This function closes a broadcast connection that has
- * 					previously been opened with p_broadcast_open().
+ *                  This function closes a broadcast connection that has
+ *                  previously been opened with p_broadcast_open().
  *
- * 					This function typically is called as an exit handler.
- *
+ * 	                This function typically is called as an exit handler.
  */
 void p_broadcast_close(struct p_broadcast_conn *c);
 
 /**
- * \brief			Send an identified best-effort broadcast packet
- * \param c			The broadcast connection on which the packet should be sent
- * \param data		A pointer to the data to be sent
- * \param length	The length of the data to be sent
- * \retval			Non-zero if the packet could be sent, zero otherwise
+ * \brief           Send an identified best-effort broadcast packet
+ * \param c         The broadcast connection on which the packet should be sent
+ * \param data      A pointer to the data to be sent
+ * \param length    The length of the data to be sent
+ * \retval          Non-zero if the packet could be sent, zero otherwise
  *
- * 					This function sends an identified best-effort broadcast
- * 					packet. It will manage the packetbuf.
+ *                  This function sends an identified best-effort broadcast
+ *                  packet. It will manage the packetbuf.
  *
- * 					The parameter c must point to a broadcast connection that
- * 					must have previously been set up with p_broadcast_open().
- *
+ *                  The parameter c must point to a broadcast connection that
+ *                  must have previously been set up with p_broadcast_open().
  */
 int p_broadcast_send(struct p_broadcast_conn *c, void *data, size_t length);
 
