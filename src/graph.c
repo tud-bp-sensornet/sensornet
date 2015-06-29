3f871843047f8cc5dbfb919353f6a85db441c427
DasMoof <marvinpeterhaus5892@yahoo.de>
2015-06-29 23:04:34 +0200

    add_node/edge now update already existing information. More tests. More comments.

--- src/graph.c
+++ src/graph.c
@@ -6,7 +6,7 @@
 
 #include "graph.h"
 
-//Static memory containing the saved node and egde information of the graph created by the k-hop-algorithm
+//Static memory containing the saved node and edge information of the graph created by the k-hop-algorithm
 MEMB(node_memory, p_node_t, MAX_NODES);
 MEMB(edge_memory, p_edge_t, MAX_EDGES);
 
@@ -16,11 +16,11 @@ static p_node_t** node_memory_array;
 static p_edge_t** edge_memory_array;
 
 //Counters for how many nodes and edges are currently saved
-static uint8_t node_count = 0;
-static uint8_t edge_count = 0;
+static uint8_t node_count;
+static uint8_t edge_count;
 
 /**
- * Needs to be called at the start of main once to initialize the memory
+ * \brief Needs to be called at the start of main once to initialize the memory
  */
 void init_graph()
 {
@@ -28,6 +28,8 @@ void init_graph()
 	memb_init(&edge_memory);
 	node_memory_array = (p_node_t**) malloc(sizeof(p_node_t*) * MAX_NODES);
 	edge_memory_array = (p_edge_t**) malloc(sizeof(p_edge_t*) * MAX_EDGES);
+	node_count = 0;
+	edge_count = 0;
 }
 
 /**************************************************************/
@@ -35,14 +37,19 @@ void init_graph()
 /**************************************************************/
 
 /**
- * Adds a new node to the memory
- * node: The node to be added to the memory
+ * \brief Adds a new node to the memory
+ * \param node The node to be added to the memory
+ *
+ * Adds a new node to the memory, if the node isn't saved already and memory could be allocated.
+ * If the node is already in memory, the known information will be overwritten with the new information.
  */
 void add_node(const p_node_t node)
 {
-	//Exit the function early, if the node already exists
-	if (find_node(&(node.addr)) != NULL)
+	//Exit the function early, if the node already exists and the contents got updated with the information of the new node
+	p_node_t *search_node = find_node(&(node.addr));
+	if (search_node != NULL)
 	{
+        memcpy(search_node, &node, sizeof(p_node_t));
 		return;
 	}
 	p_node_t *allocated_node = (p_node_t*) memb_alloc(&node_memory);
@@ -67,8 +74,8 @@ void add_node(const p_node_t node)
 }
 
 /**
- * Removes the node with the specified rimeaddr from the memory and frees the memory used by the node
- * addr: Pointer to the rimeaddr
+ * \brief Removes the node with the specified rimeaddr from the memory and frees the memory used by the node
+ * \param addr Pointer to the rimeaddr
  */
 void remove_node(const rimeaddr_t *addr)
 {
@@ -92,9 +99,12 @@ void remove_node(const rimeaddr_t *addr)
 }
 
 /**
+ * \brief Returns an array of pointers to the saved nodes in memory. The array should neither be freed, nor changed outside of graph.c.
+ * \param count A pointer to an uint8 value that will be set to the current amount of saved nodes
+ * \retval The pointer to the array of p_node_t*
+ *
  * Returns an array of pointers to the saved nodes in memory. The array should neither be freed, nor changed outside of graph.c.
- * count: A pointer to an uint8 value that will be set to the current amount of saved nodes
- * return: The pointer to the array of p_node_t*
+ * The free spaces (NULL pointers) will always be located after the filled spaces.
  */
 p_node_t **get_all_nodes(uint8_t *count)
 {
@@ -103,9 +113,9 @@ p_node_t **get_all_nodes(uint8_t *count)
 }
 
 /**
- * Returns a pointer to the node with the specified rimeaddr. If the node does not exist, NULL is returned.
- * addr: Pointer to the rimeaddr
- * return: Pointer to the node
+ * \brief Returns a pointer to the node with the specified rimeaddr. If the node does not exist, NULL is returned.
+ * \param addr Pointer to the rimeaddr
+ * \retval Pointer to the node
  */
 p_node_t *find_node(const rimeaddr_t *addr)
 {
@@ -121,8 +131,8 @@ p_node_t *find_node(const rimeaddr_t *addr)
 }
 
 /**
- * Returns how many nodes are currently saved
- * return: The amount of nodes
+ * \brief Returns how many nodes are currently saved
+ * \retval The amount of nodes
  */
 uint8_t get_node_count()
 {
@@ -135,14 +145,19 @@ uint8_t get_node_count()
 
 
 /**
- * Adds a new edge to the memory
- * edge: The edge to be added to the memory
+ * \brief Adds a new edge to the memory
+ * \param edge The edge to be added to the memory
+ *
+ * Adds a new edge to the memory, if the edge isn't saved already and memory could be allocated.
+ * If the edge is already in memory, the known information will be overwritten with the new information.
  */
 void add_edge(const p_edge_t edge)
 {
-	//Exit the function early, if the edge already exists
-	if (find_edge(&(edge.src), &(edge.dst)) != NULL)
+	//Exit the function early, if the edge already exists and the contents got updated with the information of the new edge
+	p_edge_t *search_edge = find_edge(&(edge.src), &(edge.dst));
+	if (search_edge != NULL)
 	{
+        memcpy(search_edge, &edge, sizeof(p_edge_t));
 		return;
 	}
 	p_edge_t *allocated_edge = (p_edge_t*) memb_alloc(&edge_memory);
@@ -167,9 +182,9 @@ void add_edge(const p_edge_t edge)
 }
 
 /**
- * Removes the edge with the two specified rimeaddr from the memory and frees the memory used by the edge
- * src: Pointer to the rimeaddr of the source node
- * dst: Pointer to the rimeaddr of the destination node
+ * \brief Removes the edge with the two specified rimeaddr from the memory and frees the memory used by the edge
+ * \param src Pointer to the rimeaddr of the source node
+ * \param dst Pointer to the rimeaddr of the destination node
  */
 void remove_edge(const rimeaddr_t *src, const rimeaddr_t *dst)
 {
@@ -193,9 +208,12 @@ void remove_edge(const rimeaddr_t *src, const rimeaddr_t *dst)
 }
 
 /**
+ * \brief Returns an array of pointers to the saved edges in memory. The array should neither be freed, nor changed outside of graph.c.
+ * \param count A pointer to an uint8 value that will be set to the current amount of saved edges
+ * \retval The pointer to the array of p_edge_t*
+ *
  * Returns an array of pointers to the saved edges in memory. The array should neither be freed, nor changed outside of graph.c.
- * count: A pointer to an uint8 value that will be set to the current amount of saved edges
- * return: The pointer to the array of p_edge_t*
+ * The free spaces (NULL pointers) will always be located after the filled spaces.
  */
 p_edge_t **get_all_edges(uint8_t *count){
 	*count = edge_count;
@@ -203,11 +221,14 @@ p_edge_t **get_all_edges(uint8_t *count){
 }
 
 /**
- * Returns an array of pointers to the outgoing edges of the node with the specified rimeaddr
+ * \brief Returns an array of pointers to the outgoing edges of the node with the specified rimeaddr. Must be freed.
+ * \param src_addr Pointer to the rimeaddr of the node
+ * \param count A pointer to an uint8 value that will be set to the amount of outgoing edges
+ * \retval Pointer to an array of pointers to edges. Returns NULL if there are no edges.
+ *
+ * Returns an array of pointers to the outgoing edges of the node with the specified rimeaddr.
+ * If no outgoing edges for the node were found, NULL is returned. In this case no free is needed.
  * WARNING: The returned array has to be freed after the function was called.
- * src_addr: Pointer to the rimeaddr of the node
- * count: A pointer to an uint8 value that will be set to the amount of outgoing edges
- * return: Pointer to an array of pointers to edges
  */
 p_edge_t **get_outgoing_edges(const rimeaddr_t *src_addr, uint8_t *count)
 {
@@ -246,11 +267,14 @@ p_edge_t **get_outgoing_edges(const rimeaddr_t *src_addr, uint8_t *count)
 }
 
 /**
+ * \brief Returns an array of pointers to the ingoing edges of the node with the specified rimeaddr. Must be freed.
+ * \param dst_addr Pointer to the rimeaddr of the node
+ * \param count A pointer to an uint8 value that will be set to the amount of ingoing edges
+ * \retval Pointer to an array of pointers to edges. Returns NULL if there are no edges.
+ *
  * Returns an array of pointers to the ingoing edges of the node with the specified rimeaddr
+ * If no ingoing edges for the node were found, NULL is returned. In this case no free is needed.
  * WARNING: The returned array has to be freed after the function was called.
- * dst_addr: Pointer to the rimeaddr of the node
- * count: A pointer to an uint8 value that will be set to the amount of ingoing edges
- * return: Pointer to an array of pointers to edges
  */
 p_edge_t **get_ingoing_edges(const rimeaddr_t *dst_addr, uint8_t *count)
 {
@@ -275,7 +299,7 @@ p_edge_t **get_ingoing_edges(const rimeaddr_t *dst_addr, uint8_t *count)
 	p_edge_t** ingoing_edge_array = (p_edge_t**) malloc(sizeof(p_edge_t*) * (*count));
 	uint8_t current_num = 0;
 
-	//insert the outgoing edges into the array to be returned
+	//insert the ingoing edges into the array to be returned
 	for (i = 0; i < edge_count; i++)
 	{
 		if (rimeaddr_cmp(&(edge_memory_array[i]->dst), dst_addr))
@@ -289,10 +313,10 @@ p_edge_t **get_ingoing_edges(const rimeaddr_t *dst_addr, uint8_t *count)
 }
 
 /**
- * Returns a pointer to the edge with the source and destination nodes that have the two specified rimeaddr. If it does not exist in the memory, return NULL
- * src: Pointer to the rimeaddr of the source node
- * dst: Pointer to the rimeaddr of the destination node
- * return:
+ * \brief Returns a pointer to the edge with the source and destination nodes that have the two specified rimeaddr. If it does not exist in the memory, return NULL
+ * \param src Pointer to the rimeaddr of the source node
+ * \param dst Pointer to the rimeaddr of the destination node
+ * \retval A pointer to the edge if it was found, NULL if it wasn't.
  */
 p_edge_t *find_edge(const rimeaddr_t *src, const rimeaddr_t *dst)
 {
@@ -309,8 +333,8 @@ p_edge_t *find_edge(const rimeaddr_t *src, const rimeaddr_t *dst)
 }
 
 /**
- * Returns how many edges are currently saved
- * return: The amount of edges
+ * \brief Returns how many edges are currently saved
+ * \retval The amount of edges
  */
 uint8_t get_edge_count()
 {
