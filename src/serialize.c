4d48046e53529797956fe7548aecd4eaf018864a
Teriame <richard_peter.mitev@stud.tu-darmstadt.de>
2015-07-01 19:02:41 +0200

    Added K local view functionality

--- src/serialize.c
+++ src/serialize.c
@@ -8,61 +8,91 @@
 
 void serialize(void (*packet_complete)(const void *packet_data, size_t length))
 {
+
+	//On K==0 no information will be exchanged
+	if (K == 0)
+	{
+		return;
+	}
+
 	//Single memory to avoid fragementation
 	//Memory layout: p_node_t src, (p_edge_t src_drain, p_node_t drain)*
-	void* memory = malloc(PACKETBUF_SIZE);
-	void* memory_ptr = memory;
+	void *memory = malloc(PACKETBUF_SIZE);
+	void *memory_ptr = memory;
 
-	uint8_t* node_count;
-	p_node_t** array_ptr_ptr = get_all_nodes(node_count);
+	//Get all reachable nodes and their hopcount
+	uint8_t reachable_count;
+	p_hop_t *hops = get_hop_counts(&reachable_count);
 
-	int i;
-	for (i = 0; i < (int)*node_count; i++)
+	//Package root (on K >= 1) (reachable in 0 hops)
+	memcpy(memory_ptr, find_node(&rimeaddr_node_addr), sizeof(p_node_t));
+	memory_ptr = memory_ptr + sizeof(p_node_t);
+
+	//Package all outgoing edges and edge destinations from root (on K >= 2) (reachable in 1 hops)
+	if (K >= 2)
 	{
-		//Package source node
-		memcpy(memory_ptr, array_ptr_ptr[i], sizeof(p_node_t));
-		memory_ptr = memory_ptr + sizeof(p_node_t);
+		package_and_send_edges(memory_ptr, &rimeaddr_node_addr);
+	} else {
+		return;
+	}
 
-		uint8_t* edge_count;
-		p_edge_t** edge_ptr_ptr = get_outgoing_edges(edge_count);
-
-		//Package every edge and drain
-		int j = sizeof(p_node_t);
-		int k;
-		for (k = 0; k < (int)*edge_count; k++)
+	//Package nodes and their edges reachable in up to K-1 hops
+	uint8_t l;
+	for (l = 0; l < reachable_count; l++)
+	{
+		if (hops[l].hop_count <= K - 1)
 		{
-			//Pack outgoing edge
-			memcpy(memory_ptr, edge_ptr_ptr[k], sizeof(p_edge_t));
-			memory_ptr = memory_ptr + sizeof(p_edge_t);
-
-			//Pack drain of edge
-			p_node_t* drain = find_node(&(edge_ptr_ptr[i]->dst));
-			memcpy(memory_ptr, drain, sizeof(p_node_t));
+			//Package this node
+			memcpy(memory_ptr, find_node(&(hops[l].addr)), sizeof(p_node_t));
 			memory_ptr = memory_ptr + sizeof(p_node_t);
-
-			//Package will get too big in next iteration
-			if (j + (sizeof(p_node_t) + sizeof(p_edge_t)) < PACKETBUF_SIZE && k + 1 < (int)*edge_count)
-			{
-				//Send subgraph
-				packet_complete(memory, (size_t)j);
-
-				//Reset size and packet
-				j = sizeof(p_node_t);
-				memory_ptr = memory_ptr + sizeof(p_node_t);
-			}
-
-			j = j + (sizeof(p_node_t) + sizeof(p_edge_t));
-		}
-
-		//Send subgraph
-		packet_complete(memory, full_size);
-		
-		if(edge_ptr_ptr != NULL){
-			free(edge_ptr_ptr);
+			package_and_send_edges(memory_ptr, &(hops[l].addr));
 		}
 	}
 
+	if (edge_ptr_ptr != NULL) {
+		free(edge_ptr_ptr);
+	}
+
 	free(memory);
+}
+
+void package_and_send_edges(void *memory_ptr, rimeaddr_t *src)
+{
+	//Get all outgoing edges from that node
+	uint8_t edge_count;
+	p_edge_t **edge_ptr_ptr = get_outgoing_edges(src, &edge_count);
+
+	//Package every edge and drain
+	uint8_t j = (uint8_t)sizeof(p_node_t);
+	uint8_t k;
+	for (k = 0; k < *edge_count; k++)
+	{
+		//Pack outgoing edge
+		memcpy(memory_ptr, edge_ptr_ptr[k], sizeof(p_edge_t));
+		memory_ptr = memory_ptr + sizeof(p_edge_t);
+
+		//Pack drain of edge
+		p_node_t *drain = find_node(&(edge_ptr_ptr[i]->dst));
+		memcpy(memory_ptr, drain, sizeof(p_node_t));
+		memory_ptr = memory_ptr + sizeof(p_node_t);
+
+		//Package will get too big in next iteration
+		if (j + (sizeof(p_node_t) + sizeof(p_edge_t)) < PACKETBUF_SIZE && k + 1 < *edge_count)
+		{
+			//Send subgraph
+			packet_complete(memory, (size_t)j);
+
+			//Reset size and packet
+			j = sizeof(p_node_t);
+			memory_ptr = memory_ptr + sizeof(p_node_t);
+		}
+
+		//Increment package size counter
+		j = j + (sizeof(p_node_t) + sizeof(p_edge_t));
+	}
+
+	//Send subgraph
+	packet_complete(memory, (size_t)j);
 }
 
 void deserialize(const rimeaddr_t *sender, void *packet, size_t length)
@@ -75,12 +105,12 @@ void deserialize(const rimeaddr_t *sender, void *packet, size_t length)
 	edge.ttl = 0x00; //TODO: Replace with correct ttl
 	add_edge(edge);
 
-	int i;
-	int j;
-	for (i = 0, j = 0; i < (int)length; j++)
+	uint8_t i;
+	uint8_t j;
+	for (i = 0, j = 0; i < length; j++)
 	{
 		//Memory layout: p_node_t src, (p_edge_t src_drain, p_node_t drain)*
-		if(j % 2 == 0)
+		if (j % 2 == 0)
 		{
 			//Add node or update
 			add_node((p_node_t) * (packet + i);
