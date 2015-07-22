9859a2f2923f800bf63ef493b6c611dba6823285
teriame <teriame@users.noreply.github.com>
2015-07-22 15:48:12 +0200

    Do not use subshell

--- src/test/cooja/broadcast-with-checksum/simexec.sh
+++ src/test/cooja/broadcast-with-checksum/simexec.sh
@@ -2,7 +2,9 @@ CONTIKI="../../../../lib/contiki"
 CSC="broadcast.simulation.csc"
 
 if [ ! -f $CONTIKI/tools/cooja/dist/cooja.jar ]; then
-	( cd $CONTIKI/tools/cooja; ant -q jar )
+	cd $CONTIKI/tools/cooja
+	ant -q jar
+	cd $OLDPWD
 fi
 
 java -Xshare:on -jar $CONTIKI/tools/cooja/dist/cooja.jar -nogui=$CSC -contiki=$CONTIKI > /dev/null
