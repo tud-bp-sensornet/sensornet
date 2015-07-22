e15d09a1f5ad44363d75286531205b59c4ff9da1
teriame <teriame@users.noreply.github.com>
2015-07-22 14:49:47 +0200

    Return correct exit code, test if jar exist

--- src/test/cooja/broadcast-with-checksum/simexec.sh
+++ src/test/cooja/broadcast-with-checksum/simexec.sh
@@ -1,7 +1,20 @@
 CONTIKI="../../../../lib/contiki"
 CSC="broadcast.simulation.csc"
 
+if [ ! -f $CONTIKI/tools/cooja/dist/cooja.jar ]; then
+	( cd $CONTIKI/tools/cooja; ant -q run )
+fi
+
 java -Xshare:on -jar $CONTIKI/tools/cooja/dist/cooja.jar -nogui=$CSC -contiki=$CONTIKI > /dev/null
 cat COOJA.testlog
+
+if grep -q "TEST FAILED" COOJA.testlog; then
+	rm COOJA.testlog
+	rm COOJA.log
+
+	exit 1
+fi
+
 rm COOJA.testlog
 rm COOJA.log
+exit 0
