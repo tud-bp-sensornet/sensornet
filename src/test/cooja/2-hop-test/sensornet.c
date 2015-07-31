2e2d8a81aaae9be151dbf42610896c7413e619dd
DasMoof <marvinpeterhaus5892@yahoo.de>
2015-07-31 20:21:30 +0200

    Updated the sensornet.c's of the cooja simulations to be the same as the one in src

--- src/test/cooja/2-hop-test/sensornet.c
+++ src/test/cooja/2-hop-test/sensornet.c
@@ -30,7 +30,8 @@ static struct p_broadcast_conn broadcast;
  * Testcase:Node:%d,%d\n	with the rimeaddr.u8[0] and the depth
  * Testcase:Edge:%d,%d\n	with the rimeaddr.u8[0] of the source and the rimeaddr.u8[0] of the drain
  */
-static void debug_k_hop_timer_event(void *ptr){
+static void debug_k_hop_timer_event(void *ptr)
+{
 	printf("Print Graph for Node %d\n", rimeaddr_node_addr.u8[0]);
 	uint8_t count = 0;
 
@@ -42,10 +43,10 @@ static void debug_k_hop_timer_event(void *ptr){
 	p_hop_t *hops = get_hop_counts(&count);
 	printf("Count Node hops:%d\n", count);
 	//also print root
-	printf("Testcase:Node:%d,%d\n", rimeaddr_node_addr.u8[0],0);
+	printf("Testcase:Node:%d,%d\n", rimeaddr_node_addr.u8[0], 0);
 	for (i = 0; i < count; i++)
 	{
-		printf("Testcase:Node:%d,%d\n", hops[i].addr.u8[0],hops[i].hop_count);
+		printf("Testcase:Node:%d,%d\n", hops[i].addr.u8[0], hops[i].hop_count);
 	}
 	free(hops);
 
@@ -64,7 +65,8 @@ static void
 packet_complete(const void *packet_data, size_t length)
 {
 	//Broadcast subgraph
-	if(p_broadcast_send(&broadcast, packet_data, length) == 0){
+	if(p_broadcast_send(&broadcast, packet_data, length) == 0)
+	{
 		//TODO: Errorhandling
 	}
 }
@@ -80,11 +82,11 @@ PROCESS_THREAD(simple_process, ev, data)
 	static struct etimer et;
 	init_graph();
 	
-#if __K_HOP_DEBUG__
-		static struct ctimer ct;
-		//After 45 seconds, call debug_k_hop_timer_event
-		ctimer_set(&ct, CLOCK_SECOND * 45, debug_k_hop_timer_event, NULL);
-#endif
+	#if __K_HOP_DEBUG__
+	static struct ctimer ct;
+	//After 45 seconds, call debug_k_hop_timer_event
+	ctimer_set(&ct, CLOCK_SECOND * 45, debug_k_hop_timer_event, NULL);
+	#endif
 
 	//We are root
 	p_node_t root = {rimeaddr_node_addr};
@@ -92,7 +94,8 @@ PROCESS_THREAD(simple_process, ev, data)
 
 	printf("K = %d, MAX_NODES = %d, MAX_EDGES = %d\n", (int) K, (int) MAX_NODES, (int) MAX_EDGES);
 
-	while (1) {
+	while (1)
+	{
 		/* Delay 2-4 seconds */
 		etimer_set(&et, CLOCK_SECOND * 2 + random_rand() % (CLOCK_SECOND * 4));
 
